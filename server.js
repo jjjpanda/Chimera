22aa1b33ea8ab4f96c8b3ecc926ca209c2392c42
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-07-04 14:53:36 -0400

    path changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express    = require('express')
 var serveIndex = require('serve-index')
+var path       = require('path')
 require('dotenv').config()
  
 var app = express()
@@ -7,7 +8,7 @@ var app = express()
 // Serve URLs like /ftp/thing as public/ftp/thing
 // The express.static serves the file contents
 // The serveIndex is this module serving the directory
-app.use('/', express.static(process.env.filePath), serveIndex(process.env.filePath, {'icons': true}))
+app.use('/', express.static(path.join(process.env.filePath)), serveIndex(path.join(process.env.filePath), {'icons': true}))
  
 // Listen
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 8080)
