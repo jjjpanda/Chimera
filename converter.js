a4f9ff0954478b7991317038c4cb0eef4b319bb7
jjjpanda <jawpanda13@gmail.com>
2020-07-22 12:41:34 -0400

    update files and server

---
+++ converter.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+var fs         = require('fs')
+var path       = require('path')
+var ffmpeg     = require('fluent-ffmpeg');
+const slash    = require('./slash.js')
+
+ffmpeg.setFfmpegPath(process.env.ffmpeg)
+ffmpeg.setFfprobePath(process.env.ffprobe)
+
+const dirList = fs.readdirSync(path.relative(__dirname, path.resolve(process.env.imgDir)))
+  
+console.log(dirList)
+let cameras = []
+for(let i = 0; i < process.env.cameras; i++){
+    cameras.push(`${i+1}`)
+}
+console.log(cameras)
+  
+const convert = (camera, callback) => {
+
+    let files = ""
+    if(process.env.frames != "inf"){
+        for (const file of dirList.filter(file => file.includes(".jpg") && file.split('-')[0] == camera).slice(-1 * process.env.frames)){
+        files += `file '${file}'\r\n` 
+        }
+    }
+    else{
+        for (const file of dirList.filter(file => file.includes(".jpg") && file.split('-')[0] == camera)){
+        files += `file '${file}'\r\n` 
+        }
+    }
+
+    fs.writeFileSync(path.resolve(process.env.imgDir, `img_${camera}.txt`), files)
+
+    /*
+    *
+    *
+    */
+
+    let videoCreator = ffmpeg(process.env.imgDir+`/img_${camera}.txt`).inputFormat('concat'); //ffmpeg(slash(path.join(process.env.imgDir,"img.txt"))).inputFormat('concat');
+        
+    const createVideo = (creator) => {
+        creator
+        .outputFPS(30)
+        .videoBitrate(1024)
+        .videoCodec('libx264')
+        .format('mp4')
+        .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+        })
+        .on('progress', function(progress) {
+        console.log('Processing: ' + progress.percent + '% done');
+        })
+        .on('end', function() {
+        console.log('Finished processing');
+        fs.unlinkSync(path.resolve(process.env.imgDir, `img_${camera}.txt`))
+        if(cameras.length > 0){
+            callback(cameras.pop(), convert)
+        }
+        })
+        .mergeToFile(`output_${camera}.mp4`, process.env.imgDir+'/') //.mergeToFile('output.mp4', path.relative(__dirname, path.resolve(process.env.imgDir)))
+    }
+
+    createVideo(videoCreator)
+
+}
+
+module.exports = () => {
+    convert(cameras.pop(), convert)
+}
