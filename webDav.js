2361756999034b17ff3e933034bc914ea70c685b
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-07-22 14:26:34 -0400

    updates to auth webdav

--- webDav.js
+++ webDav.js
@@ -2,15 +2,21 @@ require('dotenv').config()
 var webdav     = require('webdav-server').v2
 
 const userManager = new webdav.SimpleUserManager();
-const user = userManager.addUser(process.env.webDavUser, process.env.webDavPassword, true);
+const user = userManager.addUser(process.env.webDavUsername, process.env.webDavPassword, false);
 
 const privilegeManager = new webdav.SimplePathPrivilegeManager();
-privilegeManager.setRights(user, process.env.filePath, [ 'all' ]);
+privilegeManager.setRights(user, "/", [ 'all' ]);
 
 const server = new webdav.WebDAVServer({
     port: process.env.webdavPORT,
     httpAuthentication: new webdav.HTTPDigestAuthentication(userManager, 'Default realm'),
     privilegeManager: privilegeManager
+});
+
+server.afterRequest((arg, next) => {
+    console.log('>>', arg.request.method, arg.user.uid, arg.fullUri(), '>', arg.response.statusCode, arg.response.statusMessage);
+    console.log(arg.responseBody);
+    next();
 });
 
 module.exports = () => {
