acfddf631b415998645be741864ca1e942f296b4
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-07-22 13:44:55 -0400

    update webdav

--- webDav.js
+++ webDav.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
 var webdav     = require('webdav-server').v2
 
+const userManager = new webdav.SimpleUserManager();
+const user = userManager.addUser(process.env.webDavUser, process.env.webDavPassword, true);
+
+const privilegeManager = new webdav.SimplePathPrivilegeManager();
+privilegeManager.setRights(user, process.env.filePath, [ 'all' ]);
+
 const server = new webdav.WebDAVServer({
-    port: process.env.webdavPORT
+    port: process.env.webdavPORT,
+    httpAuthentication: new webdav.HTTPDigestAuthentication(userManager, 'Default realm'),
+    privilegeManager: privilegeManager
 });
+
 module.exports = () => {
     server.setFileSystem('/shared', new webdav.PhysicalFileSystem(process.env.filePath), (success) => {
         server.start(() => console.log('READY'));
