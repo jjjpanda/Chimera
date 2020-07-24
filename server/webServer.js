9e9eb23349a669edc6634c14e9867041c8c76a3e
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-07-24 15:53:43 -0400

    exec init

--- server/webServer.js
+++ server/webServer.js
@@ -1,49 +1,68 @@
 require('dotenv').config()
 var fs         = require('fs')
 var path       = require('path')
-var NodeSSH    = require('node-ssh')
 var express    = require('express')
-
-var ssh = new NodeSSH()
+var { exec }   = require('child_process');
 
 var app = express()
+
+app.get("/on", (req, res) => {
+    exec("tmux", [`new-session -d -s motion "motion -c /home/oo/shared/motion.conf"`], (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            res.status(200).send(JSON.stringify(error));
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.status(200).send(JSON.stringify({
+            out: stdout,
+            err: stderr
+        }))
+    });
+})
+
+app.get('/status', (req, res) => {
+    exec("pidof motion", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            res.status(200).send(JSON.stringify(error));
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.status(200).send(JSON.stringify({
+            out: stdout,
+            err: stderr
+        }))
+    });
+})
+
+app.get("/off", (req, res) => {
+    exec("pkill", [`-f motion`], (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            res.status(200).send(JSON.stringify(error));
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.status(200).send(JSON.stringify({
+            out: stdout,
+            err: stderr
+        }))
+    });
+})
  
+
+exec("pidof", ["motion"], (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+
+});
 // Listen
 module.exports = () => {
-    ssh.connect({
-        host: process.env.host,
-        username: process.env.username,
-        port: 22,
-        password: process.env.key,
-        tryKeyboard: true,
-        onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
-            console.log(name)
-            console.log(instructions)
-            console.log(instructionsLang)
-            console.log(prompts)
-            console.log(finish)
-        }
-    }).then(() => {
-        ssh.exec(
-            "ls",
-            { cwd:'/home/oo/shared' }
-        ).then((result) => {
-            console.log('STDOUT: ' + result.stdout)
-            console.log('STDERR: ' + result.stderr)
-        })
-        ssh.exec(
-            "sudo", 
-            [`tmux new-session -d -s motion "motion -c /home/oo/shared/motion.conf"`], 
-            {
-                execOptions: { pty: true },
-                stdin: `${process.env.privateKey}\n`
-            }
-        ).then((result) => {
-            console.log('STDOUT: ' + result.stdout)
-            console.log('STDERR: ' + result.stderr)
-        })
-    }, (err) => {
-        console.log(err)
-    })
-    //app.listen(process.env.PORT)
+    
+    app.listen(process.env.PORT)
 }
