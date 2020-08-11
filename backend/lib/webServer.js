e8690a32dc913b8583e705911b34c5e6215f6fbd
jjjpanda <jawpanda13@gmail.com>
2020-08-11 19:05:09 -0400

    test

--- backend/lib/webServer.js
+++ backend/lib/webServer.js
@@ -117,10 +117,18 @@ if(process.env.converter == "on"){
 
     app.post('/createZip', validateRequest, createZip)
 
-    app.post('/statusProcess', validateID, statusProcess)
-    app.post('/cancelProcess', validateID, cancelProcess)
-    app.post('/listProcess', listProcess)
-    app.post('/deleteProcess', validateID, deleteProcess)
+    app.post('/statusProcess', validateID, (req, res) => {
+        res.send("BRUH")
+    })
+    app.post('/cancelProcess', validateID, (req, res) => {
+        res.send("BRUH")
+    })
+    app.post('/listProcess', (req, res) => {
+        res.send("BRUH")
+    })
+    app.post('/deleteProcess', validateID, (req, res) => {
+        res.send("BRUH")
+    })
 }
 
 // Listen
