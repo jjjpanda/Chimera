65ce0143d2a2700e98fd5893869318b565f936c5
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-09-03 17:07:45 -0400

    media server updates to record to mp4

--- backend/lib/subServers/media.js
+++ backend/lib/subServers/media.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const NodeMediaServer = require('node-media-server')
 
 const config = {
+    logType: 1,
     rtmp: {
         port: 1935,
         chunk_size: 60000,
@@ -11,6 +12,7 @@ const config = {
     },
     http: {
         port: process.env.mediaPORT,
+        mediaroot: './media',   
         allow_origin: '*'
     },
     relay: {
@@ -19,13 +21,23 @@ const config = {
           ...process.env.cameraUsernames.split(',').map((username, index) => {
             return {
               app: 'cam',
-              mode: 'static',
+              mode: "static",
               edge: `rtsp://${username}:${process.env.cameraPasswords.split(',')[index]}@${process.env.cameraIPs.split(',')[index]}:${process.env.cameraPORTs.split(',')[index]}/cam/realmonitor?channel=1&subtype=0`,
-              name: `cam${index+1}`,
+              name: `${index+1}`,
               rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
             }
-          })
+          }),
         ]
+    },
+    trans: {
+      ffmpeg: process.env.ffmpeg,
+      tasks: [
+          {
+              app: 'cam',
+              mp4: true,
+              mp4Flags: '[movflags=frag_keyframe+empty_moov]',
+          }
+      ]
     }
 }
 
