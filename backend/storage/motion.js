9fe6a4a68c35cbbaea3b8d393b34c5d6477ab14f
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-10-02 23:56:19 -0400

    livestream fix

--- backend/storage/motion.js
+++ backend/storage/motion.js
@@ -9,11 +9,12 @@ exec(`motion -c ${process.env.motionConfPath}`)
 
 exec(`mkdir ${process.env.filePath}shared/captures/live`, () => {
     let cameraIndex = 1
-    while(process.env[`camera${cameraIndex}`] != undefined){
+    while(`camera${cameraIndex}` in process.env){
         const cameraURL = process.env[`camera${cameraIndex}`]
         console.log(`FOUND CAMERA INDEX: ${cameraIndex}`)
+        const i = cameraIndex
         exec(`mkdir ${process.env.filePath}shared/captures/live/${cameraIndex}`, () => {
-            exec(`ffmpeg -i "${cameraURL}" -fflags flush_packets -max_delay 1 -flags -global_header -hls_time 1 -hls_list_size 3 -segment_wrap 10 -hls_flags delete_segments -vcodec copy -y ${process.env.filePath}shared/captures/live/${cameraIndex}/video.m3u8`)
+            exec(`ffmpeg -i "${cameraURL}" -fflags flush_packets -max_delay 1 -flags -global_header -hls_time 1 -hls_list_size 3 -segment_wrap 10 -hls_flags delete_segments -vcodec copy -y ${process.env.filePath}shared/captures/live/${i}/video.m3u8`)
         })
         cameraIndex++
     }
