0befc34a5ccdc2eeb6e9f063a7fa775cd2eaccb8
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-10-09 11:51:48 -0400

    updates to subprocess

--- backend/storage/routes/livestream.js
+++ backend/storage/routes/livestream.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 var express    = require('express')
+const { startAllLiveStreams } = require('./lib/subprocess.js')
 
 const app = express.Router();
+
+startAllLiveStreams();
 
 app.post("/status", (req, res) => {
     exec(`ps -e | grep ffmpeg | grep rtsp`, (err, stdout, stderr) => {
--- backend/storage/routes/motion.js
+++ backend/storage/routes/motion.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 var express    = require('express')
+const { startMotion } = require('./lib/subprocess.js')
 
 const app = express.Router();
+
+startMotion();
 
 app.post("/status", (req, res) => {
     exec(`ps -e | grep motion`, (err, stdout, stderr) => {
