9a3d4357231d393877e9572aa0644fede022d471
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-10-19 22:57:00 -0400

    promises for stats

--- storage/backend/routes/file.js
+++ storage/backend/routes/file.js
@@ -59,41 +59,44 @@ const deleteFiles = (req, res) => {
     })
 }
 
-const deleteFilesBasedOnList = (req, res) => {
+const deleteFilesBasedOnCreationTime = (req, res) => {
     const list = req.body.directoryList
-    /* for(const file of list){
-        fs.unlinkSync(path.join(req.body.appendedPath, file))
-    } */
-    Promise.all(list.map(file =>
-        new Promise((resolve, reject) => {
-            fs.unlink(path.join(req.body.appendedPath, file), err => { 
-                if (err) {
-                    reject(err)
-                }
-                else{
-                    resolve('nice')
-                }
+    const checkDate = moment().subtract(req.body.days, "days")
+    if(list.length > 0){
+        Promise.all(list.map(file =>
+            new Promise((resolve, reject) => {
+                fs.stat(path.join(req.body.appendedPath, file), (error, stats) => {
+                    console.log(file, stats)
+                    if(!error && moment(stats.birthtime).isBefore(checkDate)){
+                        fs.unlink(path.join(req.body.appendedPath, file), (err) => { 
+                            if (err) {
+                                reject('deletion error')
+                            }
+                            else{
+                                console.log('deleted', file)
+                                resolve('nice')
+                            }
+                        })
+                    }
+                    else{
+                        reject('stat error or birthtime error')
+                    }
+                })
+            }).then(() => {
+                res.send({deleted: list.length > 0, count: list.length})
+            }, (reason) => {
+                res.send({error: true, reason})
             })
-        }).then(() => {
-            res.send({deleted: list.length > 0})
-        }, () => {
-            res.send({error: true})
-        })
-    ))
-    
-}
-
-const directoryFilter = (req, res, next) => {
-    const list = req.body.directoryList
-    req.body.directoryList = list.filter(file => {
-        return moment(fs.statSync(path.join(req.body.appendedPath, file)).birthtime).isBefore(moment().subtract(req.body.days, "days"))
-    })
-    next()
+        ))
+    }
+    else{
+        res.send({error: true})
+    }
 }
 
 app.post('/pathSize', validateBody, validateCameraAndAppendToPath, directoryList, fileSize) 
 app.post('/pathFileCount', validateBody, validateCameraAndAppendToPath, directoryList, fileCount)
 app.post('/pathDelete', validateBody, validateCameraAndAppendToPath, deleteFiles) 
-app.post('/pathClean', validateBody, validateCameraAndAppendToPath, validateDays, directoryList, directoryFilter, deleteFilesBasedOnList) 
+app.post('/pathClean', validateBody, validateCameraAndAppendToPath, validateDays, directoryList, deleteFilesBasedOnCreationTime) 
 
 module.exports = app
