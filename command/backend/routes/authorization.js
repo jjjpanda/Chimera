ac4df46538a786c4fa614033b12cec138fcd7b29
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-11-14 23:27:18 -0500

    sending a link to webhook with temp password

--- command/backend/routes/authorization.js
+++ command/backend/routes/authorization.js
@@ -4,6 +4,7 @@ var { validateBody, auth } = require('lib')
 const app = express.Router();
 
 app.post('/login', validateBody, auth.passwordCheck, auth.login)
+app.post('/requestLink', validateBody, auth.pinCheck, auth.generateLink)
 app.post('/verify', auth.auth, (req, res) => {
     res.json({error: false })
 })
