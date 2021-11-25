e5f634396784be950972dcfbd4c3106becfc337e
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-11-25 17:49:27 -0500

    updates (everything is broken)

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,30 @@ module.exports = {
     "jsx",
     "json"
   ],
-  setupFiles: ["dotenv/config"],
+  projects: [{
+    displayName: "command",
+    testMatch: ["<rootDir>/command/**/*.test.js"],
+    setupFiles: ["dotenv/config"]
+  },{
+    displayName: "gateway",
+    testMatch: ["<rootDir>/gateway/**/*.test.js"],
+    setupFiles: ["dotenv/config"]
+  },{
+    displayName: "lib",
+    testMatch: ["<rootDir>/lib/**/*.test.js"],
+    setupFiles: ["dotenv/config"]
+  },{
+    displayName: "livestream",
+    testMatch: ["<rootDir>/livestream/**/*.test.js"],
+    setupFiles: ["dotenv/config"]
+  },{
+    displayName: "schedule",
+    testMatch: ["<rootDir>/schedule/**/*.test.js"],
+    setupFiles: ["dotenv/config"]
+  },{
+    displayName: "storage",
+    testMatch: ["<rootDir>/storage/**/*.test.js"],
+    setupFiles: ["dotenv/config"]
+  }],
   testTimeout: 10000
 };
