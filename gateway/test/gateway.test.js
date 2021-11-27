30c9ca1874063ed69df9d46a4317e291188b9334
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-11-27 12:26:33 -0500

    gateway mock fix it doesn't even need implementation so ez pz

--- gateway/test/gateway.test.js
+++ gateway/test/gateway.test.js
@@ -3,20 +3,7 @@ const command = require('command').app
 const {handleServerStart} = require('lib')
 const gateway = require('../server.js').app
 
-jest.mock('memory', () => ({
-    client: (name) => ({
-        emit: (event, ...args) => {
-            if(event == "savePassword"){
-                args[1]()
-            }
-            else if(event == "verifyPassword"){
-                args[1](false)
-            }
-        },
-        on: () => {}
-    }),
-    server: () => {}
-}))
+jest.mock('memory')
 
 describe('Gateway Tests', () => {
     test('Gateway Timeout when proxied server is down', (done) => {
