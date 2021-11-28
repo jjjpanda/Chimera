fccca8d5443d73b28975fc172e225df3eb2f1e08
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-11-27 21:53:12 -0500

    we call this the machine crashing commit

--- livestream.config.js
+++ livestream.config.js
@@ -4,9 +4,14 @@ const path = require("path")
 const cameraURL = (i) => process.env[`livestream_CAMERA_URL_${i}`]
 
 module.exports = [{
-    script: `ffmpeg -i "${cameraURL(1)}" -fflags flush_packets -max_delay 1 -flags -global_header -hls_time 1 -hls_list_size 3 -segment_wrap 10 -filter_complex zmq,drawbox="x=10:y=20:w=200:h=60:c=red" -hls_flags delete_segments -y -c:v libx264 -crf 21 -preset veryfast ${path.join(process.env.livestream_FILEPATH, "feed", "1", "video.m3u8")}`,
-    name: `live_stream_cam_${1}`,
-    log: `./log/livestream.${1}.log`,
+    script: `false && ffmpeg -i "${cameraURL(1)}" -fflags flush_packets -max_delay 1 -flags -global_header -hls_time 1 -hls_list_size 3 -segment_wrap 10 -filter_complex zmq,drawbox="x=10:y=20:w=200:h=60:c=red" -hls_flags delete_segments -y -c:v libx264 -crf 21 -preset veryfast ${path.join(process.env.livestream_FILEPATH, "feed", "1", "video.m3u8")}`,
+    name: `live_stream_cam_box`,
+    log: `./log/livestream.box.log`,
+    log_date_format:"YYYY-MM-DD HH:mm:ss",
+}, {
+    script: `ffmpeg -y -i "${cameraURL(1)}" -update 1 -r 1 ${path.join(process.env.livestream_FILEPATH, "feed", "1", "output.jpg")}`,
+    name: `live_stream_cam_update`,
+    log: `./log/livestream.update.log`,
     log_date_format:"YYYY-MM-DD HH:mm:ss",
 }, {
     script: "server.js",
@@ -17,4 +22,9 @@ module.exports = [{
     env: {
         "NODE_ENV": "production",
     }
+}, {
+    script: "object.js",
+    name: "object",
+    log: "./log/object.log",
+    log_date_format:"YYYY-MM-DD HH:mm:ss",
 }]
