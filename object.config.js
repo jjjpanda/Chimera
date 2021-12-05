ec2ad3c6240b86553f21f0d497b544e4c0ab7e1e
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2021-12-05 16:03:44 -0500

    updates remove livestream config

--- object.config.js
+++ object.config.js
@@ -6,5 +6,16 @@ module.exports = JSON.parse(process.env.cameras).map((camera, index) => ({
     name: camera,
     inputUrl: cameraURL(index + 1),
     fps: 0.25,
-    alertUrl: process.env.alertUrl
+    alertUrl: process.env.alertUrl,
+    name: "name of input",
+
+    minimumObjectConfidence: 0.9,
+    pixelThreshold: 0.1, 
+    pixelChangePercentTolerance: 0.05, 
+
+    sizeTolerance: 0.1,
+    positionTolerance: 0.1,
+    probabilityOfExistenceDecay: 0.95,
+
+    alertUrl: 'webhook url'
 }))
