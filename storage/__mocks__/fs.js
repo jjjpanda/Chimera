0944f985cc94b324ec1b506d8daa05ca7f9264f3
J <15862136+jjjpanda@users.noreply.github.com>
2021-12-11 21:33:56 -0500

    updates for #114 (#122)

--- storage/__mocks__/fs.js
+++ storage/__mocks__/fs.js
@@ -1,7 +1,17 @@
 const path       = require("path")
 const imgDir = path.join(process.env.storage_FOLDERPATH, "shared/captures")
+const cameraDir = path.join(process.env.storage_FOLDERPATH, "shared/captures/1")
 
 const fileList = [
+    "20210101-000000-00.jpg",
+    "20210101-030000-00.jpg",
+    "20210101-060000-00.jpg",
+    "20210101-090000-00.jpg",
+    "20210101-120000-00.jpg",
+    "20210101-150000-00.jpg",
+    "20210101-180000-00.jpg",
+    "20210101-210000-00.jpg",
+    "20210102-000000-00.jpg",
     "output_1_20210101-235959_20210201-235959_video1-20210301-235959.mp4", 
     "output_1_20210101-235959_20210201-235959_video2-20210301-235959.mp4",
     "output_1_20210101-235959_20210201-235959_video3-20210301-235959.mp4",
@@ -14,7 +24,7 @@ fs.readFile = jest.fn().mockImplementation((filePath, callback) => {
     callback(false, {})
 })
 fs.readdir = jest.fn().mockImplementation((filePath, callback) => {
-    if(filePath == imgDir){
+    if(filePath == imgDir || filePath == cameraDir){
         callback(false, fileList)
     }
     else{
--- storage/test/convert.test.js
+++ storage/test/convert.test.js
@@ -4,19 +4,31 @@ const app = require('../backend/storage.js')
 var {generateID, fileName, parseFileName}    = require("../backend/routes/lib/converter.js")
 
 const fileList = [
+    "20210101-000000-00.jpg",
+    "20210101-030000-00.jpg",
+    "20210101-060000-00.jpg",
+    "20210101-090000-00.jpg",
+    "20210101-120000-00.jpg",
+    "20210101-150000-00.jpg",
+    "20210101-180000-00.jpg",
+    "20210101-210000-00.jpg",
+    "20210102-000000-00.jpg",
     "output_1_20210101-235959_20210201-235959_video1-20210301-235959.mp4", 
     "output_1_20210101-235959_20210201-235959_video2-20210301-235959.mp4",
     "output_1_20210101-235959_20210201-235959_video3-20210301-235959.mp4",
     "mp4_video3-20210301-235959.txt",
     "output_1_20210101-235959_20210201-235959_zip1-20210301-235959.zip",
 ]
-const parsedFileList = fileList.filter(file => !file.includes('txt')).map((file) => {
+
+const listOfProcesses = fileList.filter(file => file.includes('.mp4') || file.includes('.zip')).map((file) => {
     const obj = parseFileName(file)
     return {
         ...obj,
         running: obj.id.includes('video3')
     }
 })
+
+const listOfImages = fileList.filter(file => file.includes('.jpg')).map(file => `/shared/captures/1/${file}`)
 
 jest.mock('lib')
 jest.mock('fs')
@@ -31,7 +43,41 @@ describe('Convert Routes', () => {
     })
 
     describe("/convert/listFramesVideo", () => {
-        test('bruh', () => expect(2+2).toBe(4))
+        test(`get list of frame with a camera number`, (done) => {
+            supertest(app)
+            .post('/convert/listFramesVideo')
+            .send({bruh: true})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(400, { error: true, msg: "no camera" }, done)
+        })
+
+        test(`get list of frames between dates`, (done) => {
+            supertest(app)
+            .post('/convert/listFramesVideo')
+            .send({start: "20210101-000000", end: "20210102-000000", camera: 1})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(200, { list: listOfImages }, done)
+        })
+
+        test(`get list of frames between dates with no frames available`, (done) => {
+            supertest(app)
+            .post('/convert/listFramesVideo')
+            .send({start: "20210101-000000", end: "20210102-000000", camera: 2})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(200, { list: [] }, done)
+        })
+
+        test(`get list of only 3 frames between dates`, (done) => {
+            supertest(app)
+            .post('/convert/listFramesVideo')
+            .send({start: "20210101-000000", end: "20210102-000000", camera: 1, frames: 3})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(200)
+            .then(({body}) => {
+                if(body && body.list && body.list.length == 3) done()
+                else done("list should have 3 items")
+            })
+        })
     })
 
     describe("/convert/createZip", () => {
@@ -39,7 +85,40 @@ describe('Convert Routes', () => {
     })
 
     describe("/convert/statusProcess", () => {
-        test('bruh', () => expect(2+2).toBe(4))
+        test(`check status of a running process`, (done) => {
+            const id = "video3-20210301-235959"
+            supertest(app)
+            .post('/convert/statusProcess')
+            .send({id})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(200, { running: true, id }, done)
+        })
+
+        test(`check status of a finished process`, (done) => {
+            const id = "video1-20210301-235959"
+            supertest(app)
+            .post('/convert/statusProcess')
+            .send({id})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(200, { running: false, id }, done)
+        })
+
+        test(`check status of a nonexistent process`, (done) => {
+            const id = "video1-19990301-235959"
+            supertest(app)
+            .post('/convert/statusProcess')
+            .send({id})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(200, { running: false, id }, done)
+        })
+
+        test(`check status with no id`, (done) => {
+            supertest(app)
+            .post('/convert/statusProcess')
+            .send({bruh: true})
+            .set("Cookie", cookieWithBearerToken)
+            .expect(400, { error: true, msg: "no id" }, done)
+        })
     })
 
     describe("/convert/cancelProcess", () => {
@@ -67,7 +146,7 @@ describe('Convert Routes', () => {
             supertest(app)
             .get('/convert/listProcess')
             .set("Cookie", cookieWithBearerToken)
-            .expect(200, { list: parsedFileList }, done)
+            .expect(200, { list: listOfProcesses }, done)
         })
     })
 
