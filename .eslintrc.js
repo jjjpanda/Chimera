296fe50814d0ae86cfaf12f402eb88495b8f9de9
J <15862136+jjjpanda@users.noreply.github.com>
2021-12-23 13:07:10 -0500

    Bug/eslint (#131)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
-	"ignorePatterns": ["command/frontend/js/lib/*", "command/dist/app.js", "node_modules"],
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended"
-	],
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
-		},
-		"ecmaVersion": 13,
-		"sourceType": "module"
-	},
-	"plugins": [
-		"react"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"never"
-		]
-	}
+        "ignorePatterns": ["command/frontend/js/lib/*", "command/dist/app.js", "node_modules"],
+        "env": {
+                "browser": true,
+                "es2021": true,
+                "node": true
+        },
+        "extends": [
+                "eslint:recommended",
+                "plugin:react/recommended"
+        ],
+        "parserOptions": {
+                "ecmaFeatures": {
+                        "jsx": true
+                },
+                "ecmaVersion": 13,
+                "sourceType": "module"
+        },
+        "plugins": [
+                "react"
+        ],
+        "rules": {
+                "indent": [
+                        "error",
+                        "tab"
+                ],
+                "linebreak-style": [
+                        "error",
+                        "unix"
+                ],
+                "quotes": [
+                        "error",
+                        "double"
+                ],
+                "semi": [
+                        "error",
+                        "never"
+                ]
+        }
 }
